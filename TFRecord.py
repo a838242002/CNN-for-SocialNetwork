d2662a56e98f4334d2432aff1437823417de96e7
Allen Li <a838242002@gmail.com>
2018-03-27 15:13:56 +0800

    implement py code of TFRecord methods and update main.py file with TFRecord concepts

---
+++ TFRecord.py
@@ -0,0 +1,144 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+
+
+def _bytes_feature(value):
+	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _int64_feature(value):
+	return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _float_feature(value):
+	return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+def write_tfrecord(filename, data, labels):
+	compression = tf.python_io.TFRecordCompressionType.GZIP
+	tfrecords_filename = filename
+
+	writer = tf.python_io.TFRecordWriter(tfrecords_filename, options=tf.python_io.TFRecordOptions(compression))
+
+	count = 0
+	
+
+	for record in data:
+		
+
+		height = data.shape[1]
+		width = data.shape[2]
+
+		label = labels[count, 0]
+		image_string = record.tostring()
+
+		# print('height >> {0}, width >> {1}'.format(height, width))
+		example = tf.train.Example(features=tf.train.Features(feature={
+			'height': _int64_feature(height),
+			'width': _int64_feature(width),
+			'image_string': _bytes_feature(image_string),
+			'label': _float_feature([label])
+			}))
+
+		writer.write(example.SerializeToString())
+		print(count)
+		count += 1
+
+	writer.close()
+
+def read_tfrecord(filename):
+	compression = tf.python_io.TFRecordCompressionType.GZIP
+	record_iterator = tf.python_io.tf_record_iterator(path=filename, options=tf.python_io.TFRecordOptions(compression))
+
+	for string_record in record_iterator:
+		example = tf.train.Example()
+
+		example.ParseFromString(string_record)
+
+		height = int(example.features.feature['height'].int64_list.value[0])
+		width = int(example.features.feature['width'].int64_list.value[0])
+		image_string = (example.features.feature['image_string'].bytes_list.value[0])
+		label = (example.features.feature['label'].float_list.value[0])
+
+		image_1d = np.fromstring(image_string, dtype=np.float32)
+		
+		image = np.reshape(image_1d, (height, width))
+		
+		plt.imshow(image)
+		plt.show()
+
+def read_and_decode(filename_queue, IMAGE_HEIGHT, IMAGE_WIDTH):
+	compression = tf.python_io.TFRecordCompressionType.GZIP
+	reader = tf.TFRecordReader(options=tf.python_io.TFRecordOptions(compression))
+
+	_, serialized_example = reader.read(filename_queue)
+
+	features = tf.parse_single_example(
+		    serialized_example,
+		    features={
+		      'height': tf.FixedLenFeature([], tf.int64),
+		      'width': tf.FixedLenFeature([], tf.int64),
+		      'image_string': tf.FixedLenFeature([], tf.string),
+		      'label': tf.FixedLenFeature([], tf.float32)
+		      })
+
+	image = tf.decode_raw(features['image_string'], tf.float32)
+
+	label = tf.cast(features['label'], tf.float32)
+
+	height = tf.cast(features['height'], tf.int32)
+	width = tf.cast(features['width'], tf.int32)
+
+	image = tf.reshape(image, [height, width, 1])
+
+	resized_image = tf.image.resize_image_with_crop_or_pad(image=image,
+		target_height=IMAGE_HEIGHT,
+		target_width=IMAGE_WIDTH)
+
+	images, labels = tf.train.shuffle_batch(
+		[resized_image, label],
+		batch_size=100,
+		capacity=1000,
+		num_threads=1,
+		min_after_dequeue=500)
+
+	return images, labels
+
+
+# if __name__ == '__main__':
+	# with open('trainning_333_1.pickle', 'rb') as file:
+	# 	data = pickle.load(file)
+	# test_data = np.repeat(np.reshape(data[:, :3330], (-1, 10, 333)), 30, axis=1).astype(np.float32)
+	
+	
+
+	# labels = data[:, 3330:]
+	# print(labels[1,:])
+	# write_tfrecord('test2.tfrecords', test_data, labels)
+	# read_tfrecord('test2.tfrecords')
+
+	# filename_queue = tf.train.string_input_producer(
+	# 	['test2.tfrecords'], num_epochs=10)
+	# images, labels = read_and_decode(filename_queue, 300, 333)
+
+	# init_op = tf.group(tf.global_variables_initializer(),
+	# 					tf.local_variables_initializer())
+	
+	# with tf.Session() as sess:
+		
+		
+	# 	sess.run(init_op)
+
+	# 	coord = tf.train.Coordinator()
+	# 	threads = tf.train.start_queue_runners(coord=coord)
+
+	# 	for i in range(3):
+
+	# 		img, lab = sess.run([images, labels])
+
+	# 		print(lab.shape)
+
+	# 	coord.request_stop()
+	# 	coord.join(threads)
+
+	
--- main.py
+++ main.py
@@ -1,35 +1,8 @@
-from VGG import inference_op, loss
 import tensorflow as tf
 import pickle
 import numpy as np
-
-def weight_variable(shape):
-	initial = tf.truncated_normal(shape=shape, stddev=0.1)
-	return tf.Variable(initial, name='w')
-
-def bias_variable(shape):
-	initial = tf.constant(0.1, shape=shape)
-	return tf.Variable(initial, name='b')
-
-def conv_op(input_op, name, kh, kw, n_out, dh, dw, p):
-	n_in = input_op.get_shape()[-1].value
-	print('>>>>>>>>>>>', n_in)
-
-	with tf.name_scope(name) as scope:
-		# kernel = tf.get_variable(scope + "w",
-		# 	shape=[kh, kw, n_in, n_out], dtype=tf.float32,
-		# 	initializer=tf.glorot_uniform_initializer(seed=1))
-		kernel = weight_variable([kh, kw, n_in, n_out])
-
-		conv = tf.nn.conv2d(input_op, kernel, (1, dh, dw, 1),
-							padding='SAME')
-		biases = tf.Variable(tf.constant(0.1, shape = [n_out],
-										 dtype=tf.float32), name='b')
-		z = tf.nn.bias_add(conv, biases)
-		activation = tf.nn.relu(z, name = scope)
-		# p += [kernel, biases]
-
-		return activation
+import AlexNet, VGG, Layers
+import TFRecord
 
 def openPickle(filename):
 	with open(filename, 'rb') as file:
@@ -39,27 +12,62 @@ def openPickle(filename):
 
 if __name__ == '__main__':
 
+	IMAGE_HEIGHT = 300
+	IMAGE_WIDTH = 333
+	IMAGE_CHANNEL = 1
+
 	with tf.name_scope('inputs'):
-		x = tf.placeholder(tf.float32, [None, 10, 333, 1], name='x_input')
+		x = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL], name='x_input')
 		y = tf.placeholder(tf.int64, [None], name='y_input')
 		keep_prob = tf.placeholder(tf.float32, name='keep_prob')
-	predictions = inference_op(x, keep_prob)
+	predictions = AlexNet.inference_op(x, keep_prob)
 
-	loss = loss(predictions, y)
-	train_op = tf.train.AdamOptimizer(0.1).minimize(loss)
+	loss = AlexNet.loss(predictions, y)
+	train_op = tf.train.AdamOptimizer(0.001).minimize(loss)
 	
-	data = openPickle('trainning_333_1.pickle')
-	
-	train_x = np.reshape(data[0:1000, :3330], (-1, 10, 333, 1))
-	train_y = np.reshape(data[0:1000, 3330:], (-1))
-	print(train_x.shape)
-	print(train_y.shape)
+	# data = openPickle('trainning_333_1.pickle')
+	logits = AlexNet.get_logits(predictions)
+	# # test = Layers.kernal_variable('conv1', [3, 3, 1, 1])
+	# train_x = np.reshape(data[0:10, :3330], (-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL))
+	# train_y = np.reshape(data[0:10,3330:], (-1))
+	# print(train_x.shape)
+	# print(train_y.shape)
 
+	# with tf.Session() as sess:
+	# 	init = tf.global_variables_initializer()
+	# 	sess.run(init)
+
+	# 	for i in range(100):
+	# 		# print(sess.run(y, feed_dict={})
+	# 		# print(sess.run(p, feed_dict={x: train_x, keep_prob:1.0}))
+	# 		# print(sess.run(logits, feed_dict={x: train_x, y: train_y,
+	# 		#  keep_prob:1.0}))
+	# 		_, loss_value = sess.run([train_op, loss], feed_dict={x: train_x, y: train_y,
+	# 		 keep_prob:1.0})
+	# 		print(loss_value)
+
+	filename_queue = tf.train.string_input_producer(
+		['test2.tfrecords'], num_epochs=10)
+	images, labels = TFRecord.read_and_decode(filename_queue, 300, 333)
+
+	
+	
 	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		for i in range(50):
-			# print(sess.run(test, feed_dict={x: train_x, p: []}))
-			# print(sess.run)
-			_, loss_value, predict = sess.run([train_op, loss, predictions], feed_dict={x: train_x, y: train_y,
-			 keep_prob:1.0})
-			print(loss_value, predict[1,:])
+		init_op = tf.group(tf.global_variables_initializer(),
+						tf.local_variables_initializer())
+		
+		sess.run(init_op)
+
+		coord = tf.train.Coordinator()
+		threads = tf.train.start_queue_runners(coord=coord)
+
+		for i in range(1000):
+			img, lab = sess.run([images, labels])
+
+			_, loss_value = sess.run([train_op, loss], feed_dict={x: img, y: lab, keep_prob:1.0})
+			# print(sess.run(logits, feed_dict={x: img, y: lab, keep_prob:1.0}))
+
+			print(loss_value)
+
+		coord.request_stop()
+		coord.join(threads)
